c46805338ccf9874b13cdecefb3c17b659f6373d
salomey tackie <salomeytackie555@gmail.com>
2025-01-26 15:30:39 +0000

    committing all changes 2.0

--- fetch-data.js
+++ fetch-data.js
@@ -3,24 +3,21 @@ const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 const dataContainer = document.getElementById('api-data');
 try {
     const response = await fetch(apiUrl);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const users = await response.json();
+        const users = await response.json();
     dataContainer.innerHTML = '';
-    const userList = '<ul>'
+    const userList = document.createElement('ul');
 users.forEach(user =>{
     const listItem = document.createElement('li');
-    listItem.textContent = user.name;
-    userList.appendChild(listItem);
+        listItem.textContent = user.name;
+        userList.appendChild(listItem);
+   
 });
 dataContainer.appendChild(userList);
 }
 catch (error) {
 dataContainer.innerHTML = '';
 dataContainer.textContent = 'Failed to load user data.';
-console.error('Error fetching user data:', error)
+console.error('Error fetching user data:', error);
 }
 };
 document.addEventListener('DOMContentLoaded', fetchUserData);
